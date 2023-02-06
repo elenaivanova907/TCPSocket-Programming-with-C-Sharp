05f24a272e02446819c0fb2afd86306a754ddb20
elenaivanova907 <124574668+elenaivanova907@users.noreply.github.com>
2023-02-06 13:20:22 +0200

    Add files via upload

---
+++ TCPClient.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace TCPClient
+{
+    class Program
+    {
+        private const int serverPort = 10572;
+        private const string serverAddress = "127.0.0.1";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            
+            TcpClient client = new TcpClient();
+            Socket sock = client.Client;
+
+            byte[] receiveBuff;
+
+            Console.Write("Enter a request: \n1\tSTART\n2\tCLOSE\n3\tAUTH\n4\tBALANCE\n5\tDEBIT\n6\tCREDIT\n");
+
+            //client input
+            string input = Console.ReadLine();
+            bool start = true;
+
+            while (start)
+            {
+
+
+                switch (input)
+                {
+                    case "1": //START
+                        client.Connect(serverAddress, serverPort);
+                        
+                        break;
+                    case "2": //CLOSE
+                            
+                            start = false;
+                            
+                            break;
+                    case "3": //AUTH
+                        Console.WriteLine("Please enter PIN: ");
+                        string PIN = Console.ReadLine();
+
+                        //send client input to server
+                        byte[] requestBuff = Encoding.ASCII.GetBytes("AUTH " + PIN);
+                        sock.Send(requestBuff);
+                        
+
+                        //recieve message from server
+                        receiveBuff = new byte[1024];
+                        int bytesRec = sock.Receive(receiveBuff);
+                        string response = Encoding.ASCII.GetString(receiveBuff, 0, bytesRec);
+
+                        if (response == "OK")
+                        {
+                            Console.WriteLine("Authenticated");
+                        }
+                        else Console.WriteLine("NOT Authenticated");
+
+                        break;
+                    case "4": //BALANCE
+                        //send client input to server
+                        requestBuff = Encoding.ASCII.GetBytes("BALANCE");
+                        sock.Send(requestBuff);
+
+                        //recieve message from server
+                        receiveBuff = new byte[1024];
+                        bytesRec = sock.Receive(receiveBuff);
+                        response = Encoding.ASCII.GetString(receiveBuff, 0, bytesRec);
+
+                        if (response.StartsWith("OK"))
+                        {
+                            Console.WriteLine(response);
+                        }
+                        else Console.WriteLine("NOT Authenticated");
+
+                        break;
+                    case "5": //DEBIT
+                       Console.WriteLine("Please provide amount: ");
+                        input = Console.ReadLine();
+
+                        //send client input to server
+                        requestBuff = Encoding.ASCII.GetBytes("DEBIT " + input);
+                        sock.Send(requestBuff);
+
+                        //recieve message from server
+                        receiveBuff = new byte[1024];
+                        bytesRec = sock.Receive(receiveBuff);
+                        response = Encoding.ASCII.GetString(receiveBuff, 0, bytesRec);
+
+                        if (response.StartsWith("OK"))
+                        {
+                            Console.WriteLine("Debit Successful!" + response);
+                        }
+                        else Console.WriteLine("Debit Fail\n" + response);
+                        break;
+
+                    case "6": //CREDIT
+                        Console.WriteLine("Please provide amount: ");
+                        input = Console.ReadLine();
+
+                        //send client input to server
+                        requestBuff = Encoding.ASCII.GetBytes("CREDIT " + input);
+                        sock.Send(requestBuff);
+
+                        //recieve message from server
+                        receiveBuff = new byte[1024];
+                        bytesRec = sock.Receive(receiveBuff);
+                        response = Encoding.ASCII.GetString(receiveBuff, 0, bytesRec);
+
+                        if (response.StartsWith("OK"))
+                        {
+                            Console.WriteLine("Credit Successful!" + response);
+                        }
+                        else Console.WriteLine("Credit Fail\n" + response);
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid comand");
+                        start = false;
+                        break;
+
+                }
+                Console.WriteLine("Please enter command: ");
+                input = Console.ReadLine();
+
+            }
+
+            }catch(Exception e)
+            {
+                Console.WriteLine("ERROR" + e.ToString());
+            }
+
+        }
+    }
+}
---
+++ TCPServer.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+
+namespace TCPServer
+{
+
+    class Account
+    {
+        public string accnum;
+        public string name;
+        public string PIN;
+        public double balance;
+        
+
+        public Account(string accnum, string name, string PIN, double balance)
+        {
+            this.accnum = accnum;
+            this.name = name;
+            this.PIN = PIN;
+            this.balance = balance;
+
+        }
+
+        
+    }
+    class Program
+    {
+        private const int port = 10572;
+
+        static Account[] accounts = {
+                    new Account("1256985632965874", "Elena Ivanova", "5694", 5000),
+                    new Account("5236945269751302", "Dimitar Kirov", "1520", 3000),
+                    new Account("1204593015671356", "Vladimir Georgiev", "8546", 10000),
+                    new Account("3201569014358762", "Vesela Koleva", "4219", 9000),
+                    new Account("7542630264809642", "Nikola Kolev", "5743", 12000),
+        };
+
+
+
+        public static Account CheckPIN(string PIN, Account[] accounts)
+        {
+            //check PIN
+            foreach (Account account in accounts)
+            {
+                if (account.PIN == PIN)
+                {
+                    return account;
+                }
+            }
+            return null;
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+      
+            TcpListener server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+             
+
+        Console.WriteLine("Started listening ...");
+
+            while (true)
+            {
+                // wait for a client connection
+                TcpClient client = server.AcceptTcpClient();
+
+                Console.WriteLine("client.Connected: " + client.Connected);
+                // handle the communication with the client in a separate thread
+                Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
+                    t.Start(client);
+            }
+        }
+
+        public static void HandleClient(object param)
+        {
+
+            try
+            {
+
+            TcpClient client = (TcpClient)param;
+            Socket sock = client.Client;
+            string clientId = sock.RemoteEndPoint.ToString();
+
+            Console.WriteLine("Accepted client connection from {0}", clientId);
+                bool clientAuth = false;
+                Account accAuth = null;
+
+                byte[] bytes;
+                int bytesRec;
+                string request ="";
+                string response ="";
+                byte[] replyBuffer;
+
+            while (true)
+            {
+                    
+                    //recieve message
+                    bytes = new byte[1024];
+                bytesRec = sock.Receive(bytes);
+                request = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                response = "";
+                    
+
+                    if (request != null)
+                    {
+                        Console.WriteLine("Received: " + request);
+
+                        if (request.StartsWith("AUTH"))
+                        {
+
+                                string PIN = request.Split(" ")[1];
+                                accAuth = CheckPIN(PIN, accounts);
+
+                                if (accAuth != null)
+                                {
+                                    clientAuth = true;
+                                    //send response
+                                    response = "OK";
+                                    replyBuffer = Encoding.ASCII.GetBytes(response);
+                                    sock.Send(replyBuffer);
+
+                                }
+                                else
+                                {
+                                    clientAuth = false;
+                                    //send response
+                                    response = "NOTOK";
+                                    replyBuffer = Encoding.ASCII.GetBytes(response);
+                                    sock.Send(replyBuffer);
+
+                                }
+
+                        }
+                        if(request == "BALANCE")
+                        {
+                            if (clientAuth)
+                            {
+                               
+                                //send response
+                                response = "OK\nName: " + accAuth.name + " Number: " + accAuth.accnum + " Balance: " + accAuth.balance;
+                                replyBuffer = Encoding.ASCII.GetBytes(response);
+                                sock.Send(replyBuffer);
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("ERROR You are not Authenticated!");
+                                //send response
+                                response = "NOTOK";
+                                replyBuffer = Encoding.ASCII.GetBytes(response);
+                                sock.Send(replyBuffer);
+                            }
+                                
+                            
+                        }
+                        if (request.StartsWith("DEBIT"))
+                        {
+
+                            if (clientAuth)
+                            {
+                                double amount = double.Parse(request.Split(" ")[1]);
+
+                                if (accAuth.balance >= amount)
+                                {
+                                    accAuth.balance = accAuth.balance - amount;
+                                    //send response
+                                    response = "OK. Now the Account holds: " + accAuth.balance;
+                                    replyBuffer = Encoding.ASCII.GetBytes(response);
+                                    sock.Send(replyBuffer);
+                                }
+                                else
+                                {
+                                    //send response
+                                    response = "NOTOK";
+                                    replyBuffer = Encoding.ASCII.GetBytes(response);
+                                    sock.Send(replyBuffer);
+                                }
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("ERROR You are not Authenticated!");
+                                //send response
+                                response = "NOT AUTHENTICATED";
+                                replyBuffer = Encoding.ASCII.GetBytes(response);
+                                sock.Send(replyBuffer);
+                            }
+                            
+                                
+                            
+                        }
+                        if (request.StartsWith("CREDIT"))
+                        {
+                            if (clientAuth)
+                            {
+                                double amount = double.Parse(request.Split(" ")[1]);
+                                accAuth.balance = accAuth.balance + amount;
+                                //send response
+                                response = "OK. Now the Account holds: " + accAuth.balance;
+                                replyBuffer = Encoding.ASCII.GetBytes(response);
+                                sock.Send(replyBuffer);
+                            }
+                            else
+                            {
+                                Console.WriteLine("ERROR You are not Authenticated!");
+                                //send response
+                                response = "NOT AUTHENTICATED";
+                                replyBuffer = Encoding.ASCII.GetBytes(response);
+                                sock.Send(replyBuffer);
+                            }
+                           
+                               
+                            
+                        }else if(!(request.StartsWith("CREDIT")|| request.StartsWith("DEBIT")|| request.StartsWith("AUTH")|| request.StartsWith("BALANCE")))
+                        {
+                            Console.WriteLine("ERROR Unkown request");
+                        }
+
+                    }
+            }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR " + e.ToString());
+            }
+
+        }
+    }
+
+    
+    
+}
+
